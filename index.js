aa2a0eba58d489d0e36cb926b76c9509a0a3cd89
Peter Johanson <peter@peterjohanson.com>
2015-05-27 01:36:29 -0400

    Don't use chai-as-promised internally.
    * Leave power to caller to invoke in combination with
      chai-as-promised 'eventually' if so desired.

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ module.exports = function(chai, utils) {
     }
 
     if(utils.flag(this,'chai-hy-res:resolved')) {
-      new Assertion(obj.$promise).to.eventually.have.property('$resolved').be.true;
-      new Assertion(obj.$promise).to.eventually.have.property('$error').be.null;
-      new Assertion(obj.$promise).to.eventually.eql(obj);
+      new Assertion(obj).to.have.property('$resolved').be.true;
+      new Assertion(obj).to.have.property('$error').be.null;
+      new Assertion(obj).to.eql(obj);
     }
   });
 
